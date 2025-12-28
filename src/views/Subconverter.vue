c4cfa95286385838013f838546b9eb7398d0d2e9
litbpfmtw <84828673+litbpfmtw@users.noreply.github.com>
2025-12-29 00:31:00 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -348,7 +348,7 @@ export default {
           "sub-web作者提供": "https://api.wcc.best",
         },
         backendOptions: [
-          { value: "https://subapi.freeathena.hidns.vip" },
+          { value: "https://apisub.gahana.to" },
           { value: "https://subapi.cmliussss.net" },
           { value: "https://subapi.fxxk.dedyn.io" },
           { value: "https://url.v1.mk" },
@@ -1312,5 +1312,6 @@ export default {
   }
 };
 </script>
+
 
 
